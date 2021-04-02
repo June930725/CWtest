2180e577e8eb1e48252df52b728995f97cc96705
June <hilight0725@gmail.com>
2021-04-02 22:46:53 +0800

    新增 carousel

--- js/index.js
+++ js/index.js
@@ -1,24 +1,18 @@
 // Initialize Swiper
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
-  spaceBetween: 10,
-  // init: false,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+    576: {
+      slidesPerView: 1,
     },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
+    992: {
       slidesPerView: 5,
-      spaceBetween: 50,
-    },
+    }
   }
-});
+})
+
